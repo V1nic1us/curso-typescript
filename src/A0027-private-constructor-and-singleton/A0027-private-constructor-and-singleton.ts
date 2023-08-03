3ee6b643fdf913a732e3122aa6860c8804c672f7
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-08-02 23:53:23 -0400

    feat: class from private constructor and singleton

---
+++ src/A0027-private-constructor-and-singleton/A0027-private-constructor-and-singleton.ts
@@ -0,0 +1,32 @@
+// Singleton - Gof | Factory Method - Gof
+
+export class Database {
+  private static database: Database;
+
+  private constructor(
+    private host: string,
+    private user: string,
+    private password: string,
+  ) {}
+
+  connect(): void {
+    console.log(`Conectado: ${this.host}, ${this.user}, ${this.password}`);
+  }
+
+  static getDatabase(host: string, user: string, password: string): Database {
+    if (Database.database) return Database.database;
+    Database.database = new Database(host, user, password);
+    return Database.database;
+  }
+}
+
+// const db1 = new Database('localhost', 'root', '123456');
+const db1 = Database.getDatabase('localhost', 'root', '123456');
+db1.connect();
+// const db2 = new Database('localhost', 'root', '123456');
+const db2 = Database.getDatabase('localhost', 'root', '123456');
+db2.connect();
+
+console.log(db1 === db2);
+console.log(db1);
+console.log(db2);
