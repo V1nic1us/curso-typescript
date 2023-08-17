e87ccc360029d771d8c0976ef4f3c30af386ca94
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-08-17 00:44:52 -0400

    feat: class from dependency import with types

---
+++ src/A0061-dependency-import/A0061-dependency-import.ts
@@ -0,0 +1,5 @@
+import validator from 'validator';
+// import _ from 'lodash'; npm i -D @types/lodash instalar as tipagens da biblioteca
+
+console.log(validator.isEmail('marcus@gmail.com'));
+// console.log(_.clone({ name: 'Marcus' }));
